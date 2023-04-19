e622e21f31f60a91320b439f80aedd70ffe0c979
rohanzonneveld <rohanzonneveld@gmail.com>
2023-04-19 18:13:04 +0200

    Q3

--- problem3.py
+++ problem3.py
@@ -10,30 +10,40 @@ DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
 
 import numpy as np
 from sklearn.preprocessing import normalize
-from generate import GENERATE
-import random
 
-
-vocab = codecs.open("brown_vocab_100.txt")
+vocab = open("brown_vocab_100.txt")
 
 #load the indices dictionary
 word_index_dict = {}
 for i, line in enumerate(vocab):
-    #TODO: import part 1 code to build dictionary
+    word = line.rstrip()
+    word_index_dict[word] = i
 
-f = codecs.open("brown_100.txt")
+f = open("brown_100.txt")
 
+counts = np.zeros((len(word_index_dict),len(word_index_dict)))
 
-counts = #TODO: initialize numpy 0s array
+# iterate through file and update counts
+for sentence in f:
+    sentence = sentence.lower()
+    sentence = sentence.split()
+    
+    previous_word = '<s>'
+    for word in sentence[1:]:
+        counts[word_index_dict[previous_word]][word_index_dict[word]] += 1
+        previous_word = word
 
+# normalize counts
+probs = normalize(counts, norm='l1', axis=1)
 
-#TODO: iterate through file and update counts
-
-#TODO: normalize counts
-
-
-#TODO: writeout bigram probabilities
-
-
+# writeout bigram probabilities
+answer = probs[word_index_dict['all']][word_index_dict['the']]
+print(f'p(the | all) = {answer}')
+answer = probs[word_index_dict['the']][word_index_dict['jury']]
+print(f'p(jury | the) = {answer}')
+answer = probs[word_index_dict['the']][word_index_dict['campaign']]
+print(f'p(campaign | the) = {answer}')
+answer = probs[word_index_dict['anonymous']][word_index_dict['calls']]
+print(f'p(calls | anonymous) = {answer}')
 
 f.close()
